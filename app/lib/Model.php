21f6e65abf3e902e6c9a2fb5460af69b549a1cf4
bioha33ard <wolfq7522@gmail.com>
2021-11-30 15:01:40 +0200

    Create Select method

--- app/lib/Model.php
+++ app/lib/Model.php
@@ -2,33 +2,65 @@
 
 namespace app\lib;
 
-use JsonException;
 use PDO;
-use PDOStatement;
+use PDOStatement, JsonException, Throwable;
 
 class Model extends Database
 {
     /**
      * @throws JsonException
      */
-    public function query(string $sql, array $data = null): bool|int|PDOStatement
-    {
+    public function query(
+        string $sql,
+        array $data = null
+    ): bool|int|PDOStatement {
         $query = $this->connect()->prepare($sql);
         try {
             if (isset($data)) {
                 $data = array_values($data);
                 $query->execute($data);
-            }else{
+            } else {
                 $query->execute();
             }
             $query->setFetchMode(PDO::FETCH_OBJ);
             return $query;
-        } catch (\Throwable $throwable) {
+        } catch (Throwable $throwable) {
             return failed(
                 $throwable->getMessage(),
                 $throwable->getCode(),
                 $throwable->getTrace(),
             );
         }
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function select(string $table, array $data): bool|PDOStatement
+    {
+        $columns = array_keys($data);
+        $condition = $this->condition($columns);
+        try {
+            $query = $this->connect()->prepare(
+                "SELECT * FROM $table WHERE $condition"
+            );
+            $query->execute($data);
+            $query->setFetchMode(PDO::FETCH_OBJ);
+            return $query;
+        } catch (Throwable $throwable) {
+            return failed(
+                $throwable->getMessage(),
+                $throwable->getCode(),
+                $throwable->getTrace(),
+            );
+        }
+    }
+
+    protected function condition($data)
+    {
+        $keys = array_map(static fn($items) => "$items = :$items", $data);
+        (count($data) > 1) ? $condition = implode(' AND ', $keys)
+            : $condition = implode('', $keys);
+        return $condition;
     }
 }
--- index.php
+++ index.php
@@ -8,3 +8,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 $env = new Dotenv();
 $env->load(__DIR__ . '/.env');
+
+$data = ['first_name' => 'Maxim'];
+$result = (new Model())->select('users', $data);
+
+foreach ($result as $key){
+     var_dump($key);
+}
